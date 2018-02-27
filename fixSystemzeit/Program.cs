8aef061afd305321aa697a70047d78bfbbd543ca
Blubbll <mail@Blubbll.me>
2018-02-27 22:56:11 +0100

    add

---
+++ fixSystemzeit/Program.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fixSystemzeit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = "";
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://www.uhrzeit.org/time/sync.php");
+
+                dynamic stuff = JsonConvert.DeserializeObject(wc.DownloadString("https://www.uhrzeit.org/time/sync.php"));
+
+                string epoc = stuff.time;
+
+
+                var test1 = getDatum(epoc);
+                var test2 = getTime(epoc);
+
+                setDate(getDatum(epoc));
+
+               setTime(getTime(epoc));
+
+            }
+
+            
+        }
+
+
+             static void setDate(string dateInYourSystemFormat)
+        {
+            var proc = new System.Diagnostics.ProcessStartInfo();
+            proc.UseShellExecute = true;
+            proc.WorkingDirectory = @"C:\Windows\System32";
+            proc.CreateNoWindow = true;
+            proc.FileName = @"C:\Windows\System32\cmd.exe";
+            proc.Verb = "runas";
+            proc.Arguments = "/C date " + dateInYourSystemFormat;
+            try
+            {
+                System.Diagnostics.Process.Start(proc);
+            }
+            catch
+            {
+                Console.WriteLine("Konnte Uhrzeit nicht fixen");
+            }
+        }
+        static void setTime(string timeInYourSystemFormat)
+        {
+            var proc = new System.Diagnostics.ProcessStartInfo();
+            proc.UseShellExecute = true;
+            proc.WorkingDirectory = @"C:\Windows\System32";
+            proc.CreateNoWindow = true;
+            proc.FileName = @"C:\Windows\System32\cmd.exe";
+            proc.Verb = "runas";
+            proc.Arguments = "/C time " + timeInYourSystemFormat;
+            try
+            {
+                System.Diagnostics.Process.Start(proc);
+            }
+            catch
+            {
+                Console.WriteLine("Konnte Uhrzeit nicht fixen");
+            }
+        }
+
+
+        public static string getDatum(string epoc)
+        {
+            DateTime date = FromUnixTime(epoc);
+            //return (date.Day + "." + date.Month + "." + date.Year);
+            return date.ToString("dd.MM.yyyy");
+        }
+
+        public static string getTime(string epoc)
+        {
+            DateTime date = FromUnixTime(epoc);
+            //return (date.Hour + ":" + date.Minute + ":" + date.Second + "," + date.Millisecond);
+            return date.ToString("HH:mm:ss");
+        }
+
+        public static DateTime FromUnixTime(string unixTimeStamp)
+        {
+            DateTime exactDT = new DateTime(1970, 1, 1, 0, 0, 0).AddMilliseconds(Convert.ToDouble(unixTimeStamp));
+            return (exactDT.AddHours(1));//UTC + 1;
+        }
+
+    }
+
+}
---
+++ fixSystemzeit/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("fixSystemzeit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("fixSystemzeit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("c7492222-6b47-4e51-b86e-53b2d988f218")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
